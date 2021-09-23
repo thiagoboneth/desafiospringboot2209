2fe68fe15a6566544b8648271a130e6857074961
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-09-23 14:45:43 -0300

    Incrementado o metodo cadastraMedico e tratado excecao IOException

--- src/main/java/com/meli/desafiospringboot2209/service/VeterinarioService.java
+++ src/main/java/com/meli/desafiospringboot2209/service/VeterinarioService.java
@@ -16,6 +16,16 @@ import java.util.List;
 public class VeterinarioService {
 
     private VeterinarioPersistence persistence = new VeterinarioPersistence();
+
+    private boolean numeroRegistroNaoUtilizado(String numeroRegistro) {
+        for (Veterinario veterinario : persistence.listagem()) {
+            if (veterinario.getNumeroRegistro().equals(numeroRegistro)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     private boolean novoMedico(String numeroRegistro){
         for (Veterinario veterinario : persistence.listagem()){
             if(veterinario.getNumeroRegistro().equals(numeroRegistro)){
@@ -53,15 +63,25 @@ public class VeterinarioService {
         }
     }
 
-    public void cadastraMedico(Veterinario registro) throws IOException {
-        String[] campos = registro.toString().split(";");
+    public void cadastraMedico(Veterinario veterinario) throws IOException {
 
-        BufferedWriter bw  = new BufferedWriter(new FileWriter("db/medico.csv", true));
+        if (numeroRegistroNaoUtilizado(veterinario.getNumeroRegistro())) {
+            try {
+                String[] campos = veterinario.toString().split(";");
 
-        bw.write(campos[0]+";"+campos[1]+";"+campos[2]+";"+campos[3]+";"+campos[4]+";"+campos[5]+";"+campos[6]);
-        bw.flush();
-        bw.newLine();
-        bw.close();
+                BufferedWriter bw  = new BufferedWriter(new FileWriter("db/medico.csv", true));
 
+                bw.write(campos[0]+";"+campos[1]+";"+campos[2]+";"+campos[3]+";"+campos[4]+";"+campos[5]+";"+campos[6]);
+                bw.flush();
+                bw.newLine();
+                bw.close();
+
+                persistence.cadastro(veterinario, true);
+            } catch (IOException e) {
+                throw new RuntimeException("Erro ao cadastrar o veterianario");
+            }
+        } else {
+            throw new RuntimeException("Número de registro " + numeroRegistroNaoUtilizado(veterinario.getNumeroRegistro()) + " já cadastrado");
+        }
     }
 }
