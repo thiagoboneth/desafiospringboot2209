cc2dfc686865dfe2e8b7e951739870f3cbb7fa51
pedroallima <pedro.leao@mercadolivre.com>
2021-09-24 19:48:13 -0300

    alterando nome dos metodos do controller de consulta

--- src/main/java/com/meli/desafiospringboot2209/controller/ConsultaController.java
+++ src/main/java/com/meli/desafiospringboot2209/controller/ConsultaController.java
@@ -1,7 +1,6 @@
 package com.meli.desafiospringboot2209.controller;
 
 import com.meli.desafiospringboot2209.dto.ConsultaDTO;
-import com.meli.desafiospringboot2209.dto.VeterinarioDTO;
 import com.meli.desafiospringboot2209.persistence.ConsultaPersistence;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,14 +16,14 @@ public class ConsultaController {
     private ConsultaPersistence consultaPersistence = new ConsultaPersistence();
 
     @PostMapping("/cadastra") //---->ok<------
-    public ResponseEntity<ConsultaDTO> cadastraVeterinario(@RequestBody ConsultaDTO payLoad, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<ConsultaDTO> cadastraConsulta(@RequestBody ConsultaDTO payLoad, UriComponentsBuilder uriBuilder) {
         consultaPersistence.salvarConsultaNoArquivo(payLoad);
         URI uri = uriBuilder.path("/consultas/{codigo}").buildAndExpand(payLoad.getNumeroConsulta()).toUri();
         return ResponseEntity.created(uri).body(payLoad);
     }
 
     @PutMapping("/alterar/{codigo}")  //---->ok<------
-    public ResponseEntity<ConsultaDTO> alterarVeterinario(@RequestBody ConsultaDTO payLoad, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<ConsultaDTO> alterarConsulta(@RequestBody ConsultaDTO payLoad, UriComponentsBuilder uriBuilder) {
         consultaPersistence.alterarConsulta(payLoad);
         URI uri = uriBuilder.path("/alterado/{codigo}").buildAndExpand(payLoad.getNumeroConsulta()).toUri();
         return ResponseEntity.created(uri).body(payLoad);
@@ -36,7 +35,7 @@ public class ConsultaController {
     }
 
     @DeleteMapping("/deleta/{codigo}")
-    public void deletarConsulta (@PathVariable String codigo){
+    public void cadastroConsulta(@PathVariable String codigo){
         consultaPersistence.removerConsultaPorId(codigo);
     }
 }
