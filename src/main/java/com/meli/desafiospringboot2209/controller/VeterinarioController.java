03c51574c962355434899f80ad641fec0698ef81
pedroallima <pedro.leao@mercadolivre.com>
2021-09-24 09:45:39 -0300

    Criando o alterar do crud de veterinario

--- src/main/java/com/meli/desafiospringboot2209/controller/VeterinarioController.java
+++ src/main/java/com/meli/desafiospringboot2209/controller/VeterinarioController.java
@@ -23,8 +23,18 @@ public class VeterinarioController {
     }
 
 
-    @DeleteMapping("/deleta/{codigo}")
+    @DeleteMapping("/deleta/{codigo}")//---->ok<------
     public void cadastro(@PathVariable String codigo){
         veterinarioPersistence.removerMedicoPorId(codigo);
     }
+
+    @PutMapping("/alterar/{codigo}")
+    public ResponseEntity<VeterinarioDTO> alterarVeterinario(@RequestBody VeterinarioDTO payLoad, UriComponentsBuilder uriBuilder) {
+        veterinarioPersistence.alterarVeterinario(payLoad);
+        URI uri = uriBuilder.path("/alterado/{codigo}").buildAndExpand(payLoad.getNumeroRegistro()).toUri();
+        return ResponseEntity.created(uri).body(payLoad);
+    }
+
+
+
 }
