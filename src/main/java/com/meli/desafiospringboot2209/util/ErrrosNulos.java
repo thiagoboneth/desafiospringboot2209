38975787e76721990a702071e8b645d9d51a6ff9
pedroallima <pedro.leao@mercadolivre.com>
2021-09-27 14:48:36 -0300

    Criacao de validacoes para alterar Veterinario

---
+++ src/main/java/com/meli/desafiospringboot2209/util/ErrrosNulos.java
@@ -0,0 +1,10 @@
+package com.meli.desafiospringboot2209.util;
+
+public class ErrrosNulos {
+    public static void erros(Integer quantidadeErros, int contNull, String mensagemNull, int contOk){
+    if(contNull == quantidadeErros && contOk == 0)
+    {throw new RuntimeException("E necessario pelomenos 1 parametro alem "+mensagemNull+" para poder alterar.");}else
+            if (contNull == 0 && contOk == quantidadeErros)
+    {throw new RuntimeException("E necessario que pelomenos 1 parametro seja diferente para alterar.");}
+    }
+}
