1dd4cd779119393b17df1958d551919e1be22480
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-09-28 11:47:59 -0300

    Adicionado diretorio para testes

---
+++ src/test/java/com/meli/desafiospringboot2209/Desafiospringboot2209ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.desafiospringboot2209;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafiospringboot2209ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
