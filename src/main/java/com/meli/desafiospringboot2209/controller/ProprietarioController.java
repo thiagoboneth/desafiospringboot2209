fc91bd41318ea5a69a50c73034a6ca5c1e7c7eb6
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-10-01 19:11:18 -0300

    Implementado o método cadastraProprietario

--- src/main/java/com/meli/desafiospringboot2209/controller/ProprietarioController.java
+++ src/main/java/com/meli/desafiospringboot2209/controller/ProprietarioController.java
@@ -1,41 +1,52 @@
 package com.meli.desafiospringboot2209.controller;
 
 import com.meli.desafiospringboot2209.dto.ProprietarioDTO;
-import com.meli.desafiospringboot2209.persistence.ProprietarioPersistence;
+import com.meli.desafiospringboot2209.entity.Proprietario;
+import com.meli.desafiospringboot2209.service.ProprietarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/proprietario")
 public class ProprietarioController {
 
-    private ProprietarioPersistence proprietarioPersistence = new ProprietarioPersistence();
+    @Autowired
+    private ProprietarioService proprietarioService;
 
-    @PostMapping("/cadastrar")
+/*    @PostMapping("/cadastrarold")
     public ResponseEntity<ProprietarioDTO> cadastraProprietario(@RequestBody ProprietarioDTO payLoad, UriComponentsBuilder uriBuilder) {
         proprietarioPersistence.salvarProprietarioNoArquivo(payLoad);
         URI uri = uriBuilder.path("/proprietarios/{codigo}").buildAndExpand(payLoad.getCpf()).toUri();
         return ResponseEntity.created(uri).body(payLoad);
+    }*/
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<ProprietarioDTO> cadastraProprietario(@RequestBody ProprietarioDTO proprietarioDTO, UriComponentsBuilder uriBuilder) {
+        Proprietario proprietario = ProprietarioDTO.converte(proprietarioDTO);
+        proprietarioService.cadastrarProprietario(proprietario);
+        URI uri = uriBuilder.path("/proprietarios/{codigo}").buildAndExpand(proprietarioDTO.getCpf()).toUri();
+        return ResponseEntity.created(uri).body(proprietarioDTO);
     }
 
-    @GetMapping("/listar")
+
+/*    @GetMapping("/listar")
     public List<ProprietarioDTO> mostrarProprietario() {
         return proprietarioPersistence.buscarProprietario();
-    }
+    }*/
 
-    @PutMapping("/alterar/{cpf}")
+/*    @PutMapping("/alterar/{cpf}")
     public ResponseEntity<ProprietarioDTO> alterarProprietario(@RequestBody ProprietarioDTO payLoad, UriComponentsBuilder uriBuilder) {
         proprietarioPersistence.atualizarProprietario(payLoad);
         URI uri = uriBuilder.path("/alterado/{cpf}").buildAndExpand(payLoad.getCpf()).toUri();
         return ResponseEntity.created(uri).body(payLoad);
-    }
+    }*/
 
     @DeleteMapping("/deletar/{cpf}")
     public void removerProprietario(@PathVariable String cpf) {
-        proprietarioPersistence.removerProprietarioPorCpf(cpf);
+        proprietarioService.remover(cpf);
     }
 }
