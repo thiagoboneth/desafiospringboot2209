85cbaef47a0ec3fead432805abb12ace52d60da2
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-10-02 20:08:07 -0300

    Implementado métodos POST, PUT, GET, DELETE. Falta verficar parametros nulos

--- src/main/java/com/meli/desafiospringboot2209/controller/ProprietarioController.java
+++ src/main/java/com/meli/desafiospringboot2209/controller/ProprietarioController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/proprietario")
@@ -17,13 +18,7 @@ public class ProprietarioController {
     @Autowired
     private ProprietarioService proprietarioService;
 
-/*    @PostMapping("/cadastrarold")
-    public ResponseEntity<ProprietarioDTO> cadastraProprietario(@RequestBody ProprietarioDTO payLoad, UriComponentsBuilder uriBuilder) {
-        proprietarioPersistence.salvarProprietarioNoArquivo(payLoad);
-        URI uri = uriBuilder.path("/proprietarios/{codigo}").buildAndExpand(payLoad.getCpf()).toUri();
-        return ResponseEntity.created(uri).body(payLoad);
-    }*/
-
+    // Endpoint - Falta verificar cadastro com parametros nulos
     @PostMapping("/cadastrar")
     public ResponseEntity<ProprietarioDTO> cadastraProprietario(@RequestBody ProprietarioDTO proprietarioDTO, UriComponentsBuilder uriBuilder) {
         Proprietario proprietario = ProprietarioDTO.converte(proprietarioDTO);
@@ -32,21 +27,23 @@ public class ProprietarioController {
         return ResponseEntity.created(uri).body(proprietarioDTO);
     }
 
+    // Endpoint - OK
+    @GetMapping("/listar")
+    public List<Proprietario> mostrarProprietario() {
+        return proprietarioService.buscarProprietario();
+    }
 
-/*    @GetMapping("/listar")
-    public List<ProprietarioDTO> mostrarProprietario() {
-        return proprietarioPersistence.buscarProprietario();
-    }*/
+    // Endipoint - Falta verificar alteracao com parametros nulos
+    @PutMapping("/alterar/{cpf}")
+    public ResponseEntity<Proprietario> alterarProprietario(@RequestBody Proprietario proprietario, UriComponentsBuilder uriBuilder) {
+        proprietarioService.alterarProprietario(proprietario);
+        URI uri = uriBuilder.path("/alterado/{cpf}").buildAndExpand(proprietario.getCpf()).toUri();
+        return ResponseEntity.created(uri).body(proprietario);
+    }
 
-/*    @PutMapping("/alterar/{cpf}")
-    public ResponseEntity<ProprietarioDTO> alterarProprietario(@RequestBody ProprietarioDTO payLoad, UriComponentsBuilder uriBuilder) {
-        proprietarioPersistence.atualizarProprietario(payLoad);
-        URI uri = uriBuilder.path("/alterado/{cpf}").buildAndExpand(payLoad.getCpf()).toUri();
-        return ResponseEntity.created(uri).body(payLoad);
-    }*/
-
+    // Endipoint - Falta implementar a verificacao se o padciente esta em uma consulta, nao pode deletar o proprietario
     @DeleteMapping("/deletar/{cpf}")
     public void removerProprietario(@PathVariable String cpf) {
-        proprietarioService.remover(cpf);
+        proprietarioService.removerProprietario(cpf);
     }
 }
